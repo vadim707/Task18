0db04b3e9264c1092ad58489b5c5d3844647b7c2
Vadim <inika_7@mail.ru>
2020-11-28 23:05:34 +0400

    Подсчитано выражение

--- src/test/java/tests/Case1.java
+++ src/test/java/tests/Case1.java
@@ -1,15 +1,33 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Case1 {
 
     @Test
-    public void test1 (){
+    public void test1() {
         WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
-        driver.quit();
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
+        driver.findElement(By.xpath("//div[@jsname='j93WEe']")).click();
+        driver.findElement(By.xpath("//div[@jsname='N10B9']")).click();
+        driver.findElement(By.xpath("//div[@jsname='XSr6wc']")).click();
+        driver.findElement(By.xpath("//div[@jsname='lVjWed']")).click();
+        driver.findElement(By.xpath("//div[@jsname='qCp9A']")).click();
+        driver.findElement(By.xpath("//div[@jsname='YovRWb']")).click();
+        driver.findElement(By.xpath("//div[@jsname='KN1kY']")).click();
+        driver.findElement(By.xpath("//div[@jsname='pPHzQc']")).click();
+        driver.findElement(By.xpath("//div[@jsname='xAP7E']")).click();
+        driver.findElement(By.xpath("//div[@jsname='bkEvMb']")).click();
+        driver.findElement(By.xpath("//div[@jsname='WxTTNd']")).click();
+        driver.findElement(By.xpath("//div[@jsname='Ax5wH']")).click();
+        driver.findElement(By.xpath("//div[@jsname='Pt8tGc']")).click();
     }
+
 }
