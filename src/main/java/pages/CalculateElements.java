8108f5b5284b87e51280a6c53ec18c9be8f03828
Vadim <inika_7@mail.ru>
2020-11-29 13:58:04 +0400

    Первый кейс готов

---
+++ src/main/java/pages/CalculateElements.java
@@ -0,0 +1,62 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
+public class CalculateElements {
+
+    @FindBy(css = "input.gLFyf.gsfi")
+    public WebElement findCalculator;
+
+    @FindBy(xpath = "//div[@jsname='j93WEe']")
+    public WebElement click_bracket1;
+
+    @FindBy(xpath = "//div[@jsname='N10B9']")
+    public WebElement click_one;
+
+    @FindBy(xpath = "//div[@jsname='XSr6wc']")
+    public WebElement click_plus;
+
+    @FindBy(xpath = "//div[@jsname='lVjWed']")
+    public WebElement click_two;
+
+    @FindBy(xpath = "//div[@jsname='qCp9A']")
+    public WebElement click_bracket2;
+
+    @FindBy(xpath = "//div[@jsname='YovRWb']")
+    public WebElement click_multiply;
+
+    @FindBy(xpath = "//div[@jsname='KN1kY']")
+    public WebElement click_three;
+
+    @FindBy(xpath = "//div[@jsname='pPHzQc']")
+    public WebElement click_minus;
+
+    @FindBy(xpath = "//div[@jsname='xAP7E']")
+    public WebElement click_four;
+
+    @FindBy(xpath = "//div[@jsname='bkEvMb']")
+    public WebElement click_zero;
+
+    @FindBy(xpath = "//div[@jsname='WxTTNd']")
+    public WebElement click_split;
+
+    @FindBy(xpath = "//div[@jsname='Ax5wH']")
+    public WebElement click_five;
+
+    @FindBy(xpath = "//div[@jsname='Pt8tGc']")
+    public WebElement click_equally;
+
+    @FindBy(css = "span.vUGUtc")
+    public WebElement comparison1;
+
+    @FindBy(css = "span.qv3Wpe")
+    public WebElement comparison2;
+
+    public CalculateElements(WebDriver driver) {
+        initElements(driver, this);
+    }
+}
--- src/test/java/tests/Case1.java
+++ src/test/java/tests/Case1.java
@@ -1,33 +1,50 @@
 package tests;
-
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.CalculateElements;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Case1 {
 
-    @Test
-    public void test1() {
-        WebDriver driver = new ChromeDriver();
+    private static WebDriver driver;
+    private static CalculateElements calculateElements;
+
+    @BeforeAll
+    public static void init() {
+        driver = new ChromeDriver();
         driver.get("http://google.com");
-        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
-        driver.findElement(By.xpath("//div[@jsname='j93WEe']")).click();
-        driver.findElement(By.xpath("//div[@jsname='N10B9']")).click();
-        driver.findElement(By.xpath("//div[@jsname='XSr6wc']")).click();
-        driver.findElement(By.xpath("//div[@jsname='lVjWed']")).click();
-        driver.findElement(By.xpath("//div[@jsname='qCp9A']")).click();
-        driver.findElement(By.xpath("//div[@jsname='YovRWb']")).click();
-        driver.findElement(By.xpath("//div[@jsname='KN1kY']")).click();
-        driver.findElement(By.xpath("//div[@jsname='pPHzQc']")).click();
-        driver.findElement(By.xpath("//div[@jsname='xAP7E']")).click();
-        driver.findElement(By.xpath("//div[@jsname='bkEvMb']")).click();
-        driver.findElement(By.xpath("//div[@jsname='WxTTNd']")).click();
-        driver.findElement(By.xpath("//div[@jsname='Ax5wH']")).click();
-        driver.findElement(By.xpath("//div[@jsname='Pt8tGc']")).click();
+        calculateElements = new CalculateElements(driver);
     }
 
+    @Test
+    public void test1() {
+        calculateElements.findCalculator.sendKeys("Калькулятор", Keys.ENTER);
+        calculateElements.click_bracket1.click();
+        calculateElements.click_one.click();
+        calculateElements.click_plus.click();
+        calculateElements.click_two.click();
+        calculateElements.click_bracket2.click();
+        calculateElements.click_multiply.click();
+        calculateElements.click_three.click();
+        calculateElements.click_minus.click();
+        calculateElements.click_four.click();
+        calculateElements.click_zero.click();
+        calculateElements.click_split.click();
+        calculateElements.click_five.click();
+        calculateElements.click_equally.click();
+        assertEquals("(1 + 2) × 3 - 40 ÷ 5 =", calculateElements.comparison1.getText());
+        assertEquals("1", calculateElements.comparison2.getText());
+    }
+
+    @AfterAll
+    public static void teardown(){
+        driver.quit();
+    }
 }
